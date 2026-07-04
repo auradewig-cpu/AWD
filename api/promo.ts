843a882550f160037c15ae02c7833f83e60e9c17
auradewig-cpu <auradewig@gmail.com>
2026-07-04 10:03:50 +0700

    fix: QR verifyUrl to WA, S&K sudah ol

--- api/promo.ts
+++ api/promo.ts
@@ -105,7 +105,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
       `;
     }
 
-    const verifyUrl = `https://aldiwebdesigner.xyz/promo/status?code=${slotNumber}`;
+    const verifyUrl = `https://wa.me/6285286427559?text=Konfirmasi%20antrean%20${slotNumber}%20-%20${name}`;
     const qrDataUrl = await QRCode.toDataURL(verifyUrl, { width: 200 });
 
     return res.json({
